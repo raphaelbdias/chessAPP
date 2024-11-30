f7bfd457b93e16f11ab3e2779e49ee79b22b67ac
Raphael B. Dias <86736451+raphaelbdias@users.noreply.github.com>
2024-11-30 17:50:07 +0000

    brinig .env and .gitignore

---
+++ .venv/lib/python3.12/site-packages/berserk/__init__.py
@@ -0,0 +1,55 @@
+"""Top-level package for berserk."""
+
+from importlib import metadata
+
+berserk_metadata = metadata.metadata(__package__)
+
+
+__author__ = berserk_metadata["Author"]
+__email__ = berserk_metadata["Author-email"]
+__version__ = berserk_metadata["Version"]
+
+
+from .clients import Client
+from .types import (
+    ArenaResult,
+    BroadcastPlayer,
+    Team,
+    LightUser,
+    OnlineLightUser,
+    OpeningStatistic,
+    PaginatedTeams,
+    PuzzleRace,
+    SwissInfo,
+    SwissResult,
+)
+from .session import TokenSession
+from .session import Requestor
+from .formats import JSON
+from .formats import JSON_LIST
+from .formats import LIJSON
+from .formats import NDJSON
+from .formats import NDJSON_LIST
+from .formats import PGN
+
+__all__ = [
+    "ArenaResult",
+    "BroadcastPlayer",
+    "Client",
+    "JSON",
+    "JSON_LIST",
+    "LightUser",
+    "LIJSON",
+    "NDJSON",
+    "NDJSON_LIST",
+    "OnlineLightUser",
+    "OpeningStatistic",
+    "PaginatedTeams",
+    "PGN",
+    "PuzzleRace",
+    "Requestor",
+    "SwissInfo",
+    "SwissResult",
+    "Team",
+    "TokenSession",
+]
---
+++ .venv/lib/python3.12/site-packages/berserk/types/__init__.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from .account import AccountInformation, Perf, Preferences, Profile, StreamerInfo
+from .broadcast import BroadcastPlayer
+from .bulk_pairings import BulkPairing, BulkPairingGame
+from .challenges import Challenge
+from .common import ClockConfig, ExternalEngine, LightUser, OnlineLightUser, Variant
+from .puzzles import PuzzleRace
+from .opening_explorer import (
+    OpeningExplorerRating,
+    OpeningStatistic,
+    Speed,
+)
+from .team import PaginatedTeams, Team
+from .tournaments import ArenaResult, CurrentTournaments, SwissResult, SwissInfo
+
+__all__ = [
+    "AccountInformation",
+    "ArenaResult",
+    "BroadcastPlayer",
+    "BulkPairing",
+    "BulkPairingGame",
+    "Challenge",
+    "ClockConfig",
+    "CurrentTournaments",
+    "ExternalEngine",
+    "LightUser",
+    "OnlineLightUser",
+    "OpeningExplorerRating",
+    "OpeningStatistic",
+    "PaginatedTeams",
+    "Perf",
+    "Preferences",
+    "Profile",
+    "PuzzleRace",
+    "Speed",
+    "StreamerInfo",
+    "SwissInfo",
+    "SwissResult",
+    "Team",
+    "Variant",
+]
